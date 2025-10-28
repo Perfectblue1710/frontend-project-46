e09a59789a53e217245f8f9fca258b78960584dd
Perfectblue1710 <perfectblueee00@gmail.com>
2025-10-28 17:54:24 +0500

    Пройден 1 шаг, настроила commander и gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,8 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0');
 
-program.parse(process.argv);
+program.parse();
