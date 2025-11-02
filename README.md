3a17b1332dae5723de6993ef5fd90d9baa9fcd9e
Perfectblue1710 <perfectblueee00@gmail.com>
2025-11-02 10:33:09 +0500

    Демонстрация работы в ascinema, изменения файла genDiff

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Perfectblue1710/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Perfectblue1710/frontend-project-46/actions)
+### Запись 1(демонстрация)
+[![https://asciinema.org/a/tMrksXv1N27x16ERSuiN7sblw]]
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,19 +1,20 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import genDiff from '../src/genDiff.js';
 
 const program = new Command();
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
-  .argument('<filepath1>', 'first configuration file')
-  .argument('<filepath2>', 'second configuration file')
+  .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
- .action((filepath1, filepath2) => {
-    const result = genDiff(filepath1, filepath2);
-    console.log(result);
+  .action((filepath1, filepath2) => {
+    const diff = genDiff(filepath1, filepath2);
+    console.log(diff);
   });
 
 program.parse();
+
