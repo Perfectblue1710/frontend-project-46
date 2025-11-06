3a1dc29010e0d62be30554dc7faeedf62379e3b3
Perfectblue1710 <perfectblueee00@gmail.com>
2025-11-06 10:46:13 +0500

    подключен бейдж на сонар, создан токен, запущен eslint

---
+++ __tests__/gendiff.tests.js
@@ -0,0 +1,19 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { readFileSync } from 'fs';
+import genDiff from '../src/genDiff.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
+test('gendiff flat JSON', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+  const expected = readFile('expected.txt'); 
+
+  const result = genDiff(filepath1, filepath2);
+  expect(result).toBe(expected.trim());
+});
